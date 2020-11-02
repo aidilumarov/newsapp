a1ddca1d88d5ce3a538cada9d177f8300a32183c
aidilumarov <aidildev@tuta.io>
2020-11-02 17:13:13 +0600

    Implemented News Service

---
+++ NewsApp/NewsApp/NewsApp/Services/NewsService.cs
@@ -0,0 +1,45 @@
+﻿using NewsApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewsApp.Services
+{
+    public class NewsService
+    {
+        public async Task<NewsResult> GetNewsAsync(NewsScope scope)
+        {
+            string url = GetUrl(scope);
+
+            var webclient = new WebClient();
+            var json = await webclient.DownloadStringTaskAsync(url);
+
+            return JsonConvert.DeserializeObject<NewsResult>(json);
+        }
+
+        private string GetUrl(NewsScope scope)
+        {
+            return scope switch
+            {
+                NewsScope.Headlines => Headlines,
+                NewsScope.Local => Local,
+                NewsScope.Global => Global,
+                _ => throw new Exception("Undefined scope")
+            };
+        }
+
+        private string Headlines =>
+            "https://newsapi.org/v2/top-headlines?" +
+            "country=us&" + $"apiKey={Settings.NewsApiKey}";
+
+        private string Local =>
+            "https://newsapi.org/v2/everything?q=local&" +
+            "country=us&" + $"apiKey={Settings.NewsApiKey}";
+
+        private string Global =>
+            "https://newsapi.org/v2/everything?q=global&" +
+            "country=us&" + $"apiKey={Settings.NewsApiKey}";
+    }
+}

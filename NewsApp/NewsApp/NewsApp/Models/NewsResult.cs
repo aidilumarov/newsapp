950b8248d1f9febf8ed214d0d23bdb579e872e0a
aidilumarov <aidildev@tuta.io>
2020-11-02 16:51:19 +0600

    Added models

---
+++ NewsApp/NewsApp/NewsApp/Models/NewsResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsApp.Models
+{
+    public class NewsResult
+    {
+        public string Status { get; set; }
+
+        public int TotalResults { get; set; }
+
+        public List<Article> Articles { get; set; }
+    }
+}
---
+++ NewsApp/NewsApp/NewsApp/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsApp
+{
+    public static class Settings
+    {
+        public static string NewsApiKey
+        {
+            get
+            {
+                return "1495a11419cb44d38ae5c26c007dc67a";
+            }
+        }
+    }
+}
